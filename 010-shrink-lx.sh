e07c4122354ecdc01c63fac9b5f8b8a6dc5baaa4
Michael Weiser <michael.weiser@atos.net>
2023-02-27 15:53:53 +0100

    Adjust shrink

--- 010-shrink-lx.sh
+++ 010-shrink-lx.sh
@@ -1,11 +1,12 @@
-#/bin/bash
-dd if=/dev/zero of=/bigfile ; rm -vf /bigfile
+#/bin/bash -e
+dd if=/dev/zero of=/bigfile bs=10240k || true
+rm -vf /bigfile
 
-vmware-toolbox-cmd disk wipe /
+swapon --show=NAME,UUID | tail -n +2 | while read dev uuid ; do
+	echo "DEV: $dev, UUID: $uuid"
+	swapoff $dev
+	dd if=/dev/zero of=$dev bs=10240k || true
+	mkswap -U $uuid $dev
+done
 
-cat /etc/fstab | grep -v swap > /etc/fstab$$ && echo /dev/sda5 none swap defaults 0 0 >> /etc/fstab$$ ; cat  /etc/fstab$$ > /etc/fstab ; rm -vf /etc/fstab$$
-
-swapoff /dev/sda5
-dd if=/dev/zero of=/dev/sda5
-mkswap /dev/sda5
-reboot
+poweroff
