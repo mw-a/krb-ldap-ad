bc732a0a8354c5759e3861b928aff18ea36376f0
Michael Weiser <michael.weiser@atos.net>
2023-06-01 16:43:00 +0200

    Switch dcpromo and domain join to Powershell

--- 120-adskdc01-join-win01.cmd
+++
@@ -1,1 +0,0 @@
-netdom join win01 /Domain:ADS.EXAMPLE.COM /UserO:Administrator /PasswordO:P@ssw0rd /UserD:Administrator /PasswordD:P@ssw0rd  /Reboot:1
---
+++ 120-adskdc01-join-win01.ps1
@@ -0,0 +1,1 @@
+Add-Computer -DomainName ads.example.com -DomainCredential Administrator -Restart
