a2d39ab1a1cfd82c282b4fe3763cb969371d4c90
Michael Weiser <michael.weiser@atos.net>
2023-06-06 16:01:04 +0200

    Switch subdom dcpromo to powershell

---
+++ 150-adskdc02-dcpromo.ps1
@@ -0,0 +1,3 @@
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSDomain -DomainType "child" -NewDomainName "subdom" -NewDomainNetBIOSName "SUBDOM" -ParentDomainName "ads.example.com" -Credential (Get-Credential "ADS\Administrator") -DomainMode 7 -InstallDns:$False
