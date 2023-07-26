041c857046c261ccf01059e100b26f5ec8635f8f
Michael Weiser <michael.weiser@atos.net>
2023-07-26 14:57:38 +0200

    Work around PAC encryption type problem

--- 170-kdc01-trust.sh
+++ 170-kdc01-trust.sh
@@ -2,6 +2,6 @@
 
 kadmin -r EXAMPLE.COM -p user/admin@EXAMPLE.COM <<EOF
 P@ssw0rd
-addprinc -pw P@ssw0rd -clearpolicy krbtgt/ADS.EXAMPLE.COM@EXAMPLE.COM
+addprinc -pw P@ssw0rd -clearpolicy +no_auth_data_required krbtgt/ADS.EXAMPLE.COM@EXAMPLE.COM
 addprinc -pw P@ssw0rd -clearpolicy krbtgt/EXAMPLE.COM@ADS.EXAMPLE.COM
 EOF
